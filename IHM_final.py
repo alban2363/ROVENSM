c7ea07014c195fd0cebbaf3136c8496b55930b31
alban2363 <alban2363@gmail.com>
2025-05-15 22:00:01 +0200

    Initial commit

---
+++ IHM_final.py
@@ -0,0 +1,266 @@
+import serial
+import time
+import tkinter as tk
+
+# ------------------------------ FONCTIONS ----------------------------------------
+def fermer():
+    root.destroy()
+    ser.close()
+
+def afficher_frame(frame):
+    for f in [frame_menu, frame_parametres, frame_pilote_auto]:
+        f.pack_forget()
+    frame.pack(expand=True, fill="both")
+    
+# Envoi de la consigne (trace = 1)
+def f_send_cap_consigne():
+    try:
+        valeur = float(entry_consigne.get())
+        if 0 <= valeur <= 360:
+            envoyer_variable(1, valeur)
+            entry_consigne.delete(0,tk.END)
+        else:
+            label_valeur_consigne.config(text="Valeur invalide")
+            entry_consigne.delete(0,tk.END)
+    except ValueError:
+        label_valeur_consigne.config(text="Entrée non valide")
+        entry_consigne.delete(0,tk.END)
+
+# Mode automatique (trace = 2)
+def f_auto():
+    envoyer_variable(2, 0)
+    label_mode_auto_manu.config(text="Mode Automatique activé")
+
+# Mode manuel (trace = 3)
+def f_manu():
+    envoyer_variable(3, 0)
+    label_mode_auto_manu.config(text="Mode Manuel activé") 
+#def f_changer_kp (trace = 4)
+def f_Kp():
+    try:
+        valeur = float(entry_kp.get())
+        if valeur >= 0 and valeur<= 100 :
+            envoyer_variable(4,valeur)
+            entry_kp.delete(0,tk.END)
+        else :
+            label_affichage_kp.config(text="valeur hors plage")
+            entry_kp.delete(0,tk.END)
+    except ValueError:
+        label_affichage_kp.config(text="Entrée non valide")
+        entry_kp.delete(0,tk.END)
+
+
+# def f_changer_Ti (trace = 5 )
+def f_Ti():
+    try:
+        valeur = float(entry_ti.get())
+        if valeur >= 0 and valeur <= 500 :
+            envoyer_variable(5,valeur)
+            entry_ti.delete(0,tk.END)
+        else :
+            label_affichage_ti.config(text="valeur hors plage")
+            entry_ti.delete(0,tk.END)
+    except ValueError:
+        label_affichage_ti.config(text="Entrée non valide")
+        entry_ti.delete(0,tk.END)
+        
+        
+        
+        
+# def f_changer_alpha (trace = 6)  rapport cyclique
+def f_changer_alpha():
+    try:
+        valeur = float(entry_alpha.get())
+        if  valeur >= 0 and valeur <= 100:
+            envoyer_variable(6,valeur)
+            entry_alpha.delete(0,tk.END)
+        else:
+            label_affichage_alpha.config(text="valeur hors plage")
+            entry_alpha.delete(0,tk.END)
+    except ValueError:
+        label_affichage_alpha.config(text="Entrée non valide")
+        entry_alpha.delete(0,tk.END)
+        
+        
+        
+        
+# Envoi d'une variable vers l'Arduino
+def envoyer_variable(identifiant, valeur):
+    data = identifiant.to_bytes(1, 'little')
+    valeur_int = int(valeur * 100)  # conserver 2 décimales
+    data += valeur_int.to_bytes(2, 'little', signed=True)
+    ser.write(data)
+    time.sleep(0.2)
+
+
+# ------------------Lecture série sans bloquer l'IHM------------------------
+def lecture_serie():
+    if ser.in_waiting:
+        try:
+            line = ser.readline().decode('utf-8').strip()
+            if line:
+                cap_actuel_str, cap_consigne_str ,kp_str,ti_str,alpha_pourcent_str= line.split(',')
+                cap_actuel = float(cap_actuel_str)
+                cap_consigne = float(cap_consigne_str)
+                kp =float(kp_str)
+                ti=float(ti_str)
+                alpha_pourcent=float(alpha_pourcent_str)
+                label_valeur_consigne.config(text=f"{cap_consigne} °")
+                label_valeur_cap_actuel.config(text=f"{cap_actuel} °")
+                label_affichage_kp.config(text=f"{kp} %/°")
+                label_affichage_ti.config(text=f"{ti}s")
+                label_affichage_alpha.config(text=f"{alpha_pourcent}%")
+                
+        except ValueError:
+            pass  # ligne mal formée ignorée
+    root.after(100, lecture_serie)
+
+# ----------------------------- INTERFACE TKINTER ---------------------------------------
+
+root = tk.Tk()
+root.title("IHM ROV")
+root.geometry("1920x1040")
+root.resizable(False, False)
+
+# ---------------------- Frame Menu Principal ----------------------
+frame_menu = tk.Frame(root)
+
+label_menu = tk.Label(frame_menu, text="Menu IHM", font=("Courier", 48, "bold"))
+label_menu.pack(pady=100)
+
+frame_boutons_menu = tk.Frame(frame_menu)
+frame_boutons_menu.pack(pady=50)
+
+bouton_pilote_auto = tk.Button(frame_boutons_menu, text="Pilote Automatique", bg="#b2daf9", font=("Courier", 40), width=30,
+                               command=lambda: afficher_frame(frame_pilote_auto))
+bouton_pilote_auto.pack(pady=20)
+
+bouton_parametres = tk.Button(frame_boutons_menu, text="Paramètres", bg="#7df87b", font=("Courier", 40), width=30,
+                              command=lambda: afficher_frame(frame_parametres))
+bouton_parametres.pack(pady=20)
+
+# ---------------------- Frame Paramètres ----------------------
+frame_parametres = tk.Frame(root, bg="#7df87b")
+
+label_parametres = tk.Label(frame_parametres, text="Menu Paramètres", bg="#7df87b", font=("Courier", 40, "bold"))
+label_parametres.pack(pady=60)
+
+bouton_retour_param = tk.Button(frame_parametres, text="Retour", command=lambda: afficher_frame(frame_menu),
+                                bg='green', font=("Courier", 40))
+bouton_retour_param.pack(side="bottom", pady=30)
+
+
+# --- Section Kp ---
+frame_kp = tk.Frame(frame_parametres, bg="#7df87b")
+frame_kp.pack(pady=20)
+
+label_kp = tk.Label(frame_kp, text="Valeur Kp :", bg="#7df87b", font=("Courier", 30, "bold"))
+label_kp.pack(side="left", padx=10)
+
+entry_kp = tk.Entry(frame_kp, font=("Courier", 25), width=20)
+entry_kp.pack(side="left", padx=10)
+
+label_affichage_kp =tk.Label(frame_kp , text="En attente", bg="#7df87b", font=("Courier", 20, "bold"))
+label_affichage_kp.pack(side="left", padx=10)
+
+
+btn_valider_kp= tk.Button(frame_kp, command= f_Kp, text="Valider", font=("Courier", 30), bg='grey')
+btn_valider_kp.pack(side = "left", pady=10)
+
+# --- Section Ti ---
+frame_ti = tk.Frame(frame_parametres, bg="#7df87b")
+frame_ti.pack(pady=20)
+
+label_ti = tk.Label(frame_ti, text="Valeur Ti :", bg="#7df87b", font=("Courier", 30, "bold"))
+label_ti.pack(side="left", padx=10)
+
+entry_ti = tk.Entry(frame_ti, font=("Courier", 25), width=20)
+entry_ti.pack(side="left", padx=10)
+
+label_affichage_ti =tk.Label(frame_ti, text="En attente", bg="#7df87b", font=("Courier", 20, "bold"))
+label_affichage_ti.pack(side="left", padx=10)
+
+btn_valider_Ti= tk.Button(frame_ti, command= f_Ti, text="Valider", font=("Courier", 30), bg='grey')
+btn_valider_Ti.pack(side = "left", pady=10)
+
+# --- Rapport cyclique alpha(%) ---
+frame_alpha = tk.Frame(frame_parametres, bg="#7df87b")
+frame_alpha.pack(pady=20)
+
+label_alpha = tk.Label(frame_alpha, text="Valeur rapport cyclique alpha (%) :", bg="#7df87b", font=("Courier", 30, "bold"))
+label_alpha.pack(side="left", padx=10)
+
+entry_alpha = tk.Entry(frame_alpha, font=("Courier", 25), width=20)
+entry_alpha.pack(side="left", padx=10)
+
+label_affichage_alpha =tk.Label(frame_alpha, text="En attente", bg="#7df87b", font=("Courier", 20, "bold"))
+label_affichage_alpha.pack(side="left", padx=10)
+
+btn_valider_alpha= tk.Button(frame_alpha, command= f_changer_alpha, text="Valider", font=("Courier", 30), bg='grey')
+btn_valider_alpha.pack(side = "left", pady=10)
+# ---------------------- Frame Pilote Automatique ----------------------
+frame_pilote_auto = tk.Frame(root, bg="#b2daf9")
+
+label_pilote = tk.Label(frame_pilote_auto, text="Menu Pilote Automatique", bg="#b2daf9", font=("Courier", 40, "bold"))
+label_pilote.pack(pady=20)
+
+# -------- Section Consigne --------
+frame_consigne = tk.Frame(frame_pilote_auto, bg="#b2daf9")
+frame_consigne.pack(pady=20)
+
+label_consigne = tk.Label(frame_consigne, text="Cap Consigne (0 - 360)", bg="#b2daf9", font=("Courier", 25, "bold"))
+label_consigne.pack(pady=10)
+
+label_valeur_consigne = tk.Label(frame_consigne, text="0 °", bg="#b2daf9", font=("Courier", 20))
+label_valeur_consigne.pack(pady=10)
+
+entry_consigne = tk.Entry(frame_consigne, font=("Courier", 20), width=20)
+entry_consigne.pack()
+
+btn_valider_consigne= tk.Button(frame_consigne, command=f_send_cap_consigne, text="Valider", font=("Courier", 30), bg='grey')
+btn_valider_consigne.pack(pady=10)
+
+# -------- Section Cap actuel --------
+label_cap_actuel = tk.Label(frame_pilote_auto, text="Cap Actuel", bg="#b2daf9", font=("Courier", 25, "bold"))
+label_cap_actuel.pack(pady=20)
+
+label_valeur_cap_actuel = tk.Label(frame_pilote_auto, text="En attente", bg="#b2daf9", font=("Courier", 20))
+label_valeur_cap_actuel.pack(pady=10)
+
+# -------- Section Mode Manuel / Auto --------
+label_mode = tk.Label(frame_pilote_auto, text="Mode Manuel / Automatique", bg="#b2daf9", font=("Courier", 25, "bold"))
+label_mode.pack(pady=10)
+
+frame_modes = tk.Frame(frame_pilote_auto, bg="#b2daf9")
+frame_modes.pack(pady=10)
+
+btn_manuel = tk.Button(frame_modes, text="Manuel", command=f_manu, font=("Courier", 30), width=15, bg='grey')
+btn_manuel.grid(row=0, column=0, padx=10)
+
+btn_auto = tk.Button(frame_modes, text="Auto", command=f_auto, font=("Courier", 30), width=15, bg='grey')
+btn_auto.grid(row=0, column=1, padx=10)
+
+label_mode_auto_manu = tk.Label(frame_pilote_auto, text="Mode Manuel activé", bg="#b2daf9", font=("Courier", 20))
+label_mode_auto_manu.pack(pady=10)
+
+
+
+btn_retour_auto = tk.Button(frame_pilote_auto, text="Retour", command=lambda: afficher_frame(frame_menu),bg='green', font=("Courier", 40))
+btn_retour_auto.pack(side="bottom", pady=10)
+
+# ---------------------- Bouton Fermer commun ----------------------
+bouton_fermer = tk.Button(root, text="Fermer", command=fermer, bg='red', font=("Courier", 40))
+bouton_fermer.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
+
+# Affichage initial
+afficher_frame(frame_menu)
+
+# Initialisation port série
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+time.sleep(2)  # Attente du reboot Arduino
+
+# Lancer la lecture série périodique
+lecture_serie()
+
+# Boucle principale
+root.mainloop()
---
+++ arduinofinal/arduinofinal.ino
@@ -0,0 +1,238 @@
+//Commentaire:
+
+//Branchement capteur
+//Rouge sur 5V
+//Noir sur GND
+//SDA sur SDA
+//SCL sur SCL
+
+#include <bmm150_defs.h>
+#include <DFRobot_BMM150.h>
+#include <math.h>
+
+#include "DFRobot_BMM150.h"
+
+// Définition des broches du Motor Shield Rev3
+// channel A
+int directionPin1 = 12;
+int pwmPin1 = 3;
+int brakePin1 = 9;
+
+// channel B
+int directionPin2 = 13;
+int pwmPin2 = 11;
+int brakePin2 = 8;
+
+//boolean to switch direction
+bool directionState1 = false ;
+bool directionState2 = false ;
+
+DFRobot_BMM150_I2C bmm150(&Wire, I2C_ADDRESS_4);
+
+//Moyenne mobile
+const int N = 20;                  // Taille de la fenêtre
+float angles[N];                  // Tableau pour stocker les dernières valeurs
+int index = 0;                    // Position actuelle dans le tableau
+float total = 0;                  // Somme des valeurs
+float capActuel_filt = 0;
+
+float capActuel , ecart,ecart1, commande;          // variables du correcteur (nombres réels)
+float capConsigne=90 ;                      // variables pour générer la consigne (nombres réels)
+float capPrecedent=capConsigne ;            //utilisée pour le ramp on setpoint
+unsigned int Te, Tch;                       // variables pour la gestion du temps (nombres réels)
+unsigned long DateEch, DateChgt;            // Dates (nombres réels)
+int dt =100;                                 // durée en millisecondes souhaitée entre deux cycles (mesure -> commande)
+ 
+float kp =0.8 ;         // Gain proportionnel. Préciser la valeur
+float Ti = 50  ;       // Temps intégral. Préciser la valeur. Unités ? millisecondes
+float ui = 0  ;         // valeur initiale de l'action intégrale. Préciser la valeur. Unités ?
+
+float pente=10;         //pente de la consigne 10°/s
+bool modeAuto= false ;   //booléen pour le mode Auto ou mode Manu
+
+float alpha_pourcent =39.21;
+float alpha_bin = 100;
+
+
+#define BMM150_CS D3
+
+//DFRobot_BMM150_SPI bmm150(/*cs = */BMM150_CS);
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);{
+  while(bmm150.begin()){
+    Serial.println("bmm150 init failed, Please try again!");
+    delay(1000);
+  } Serial.println("bmm150 init success!");
+
+  bmm150.setOperationMode(BMM150_POWERMODE_NORMAL);//Normal mode
+
+  bmm150.setPresetMode(BMM150_PRESETMODE_HIGHACCURACY) ;//High-accuracy mode
+
+  bmm150.setRate(BMM150_DATA_RATE_10HZ) ;//10HZ
+
+  bmm150.setMeasurementXYZ();//Default config of X-, Y-, and Z-axis
+  
+  }
+  
+  for (int i = 0; i < N; i++) {
+    angles[i] = 0;
+  }
+
+  //define pins
+  pinMode(directionPin1, OUTPUT);
+  pinMode(pwmPin1, OUTPUT);
+  pinMode(brakePin1, OUTPUT);
+
+  pinMode(directionPin2, OUTPUT);
+  pinMode(pwmPin2, OUTPUT);
+  pinMode(brakePin2, OUTPUT);
+}
+
+void loop(){
+
+  if (Serial.available() >= 3 ) 
+    {
+    byte identifiant = Serial.read();
+    byte lowByte = Serial.read();
+    byte highByte = Serial.read();
+
+    int valeur_int = (highByte << 8) | lowByte ;
+
+    float valeur = valeur_int / 100.0 ; 
+
+    if (identifiant == 1)
+      {
+        capConsigne = valeur ;
+      }
+    if (identifiant == 2)
+      {
+        modeAuto = true ;
+      }
+    if (identifiant == 3)
+      {
+        modeAuto = false ;
+      }
+    if (identifiant == 4)
+      {
+       kp =valeur;
+      }
+    if (identifiant == 5)
+      {
+       Ti= valeur;
+      }
+    if (identifiant == 6)
+      {
+        alpha_pourcent = valeur;
+        alpha_bin =alpha_pourcent*(255/100);
+      }
+  }
+
+  Te = millis() - DateEch;                    //temps d'échantillonnage
+  
+  if ( Te >= dt) {                            // toutes les (au moins) dt millisecondes exécuter les instructions suivantes   
+    DateEch = millis();                       //date ech: temps actuel       
+    
+    capActuel = bmm150.getCompassDegree();    //donne le cap actuel de la maquette en degrès sur le nord magnétique
+
+    
+
+    total -= angles[index];              // Enlève l'ancienne valeur
+    angles[index] = capActuel;      // Remplace par la nouvelle
+    total += capActuel;             // Ajoute la nouvelle valeur
+
+    index = (index + 1) % N;             // Passe à la prochaine case
+
+    capActuel_filt = total / N;                 // Calcule la moyenne
+
+    
+    //consigne en rampe
+    if (modeAuto== true) {                              //Mode AUTO
+      //consigne en rampe(ramp on setpoint)
+      if ( capConsigne > capPrecedent ) {
+        capPrecedent += pente/(DateEch*60*1000) ;
+        if ( capConsigne <= capPrecedent ) {
+          capPrecedent = capConsigne;
+          //Serial.println(capConsigne);
+          //Serial.print(",");
+          //Serial.println(capPrecedent);
+        }
+      }
+        
+      if ( capConsigne <= capPrecedent ) {
+        capPrecedent -= pente/(DateEch*60*1000);
+        if ( capConsigne >= capPrecedent ) {
+          capPrecedent = capConsigne;
+          //Serial.print(capConsigne);
+          //Serial.println( capPrecedent );
+        }
+      }
+
+      // Implémentez ici la loi de commande PI
+      //ecart = capActuel_filt - capPrecedent   ;         //calcul de l'ecart entre le cap actuel et le cap consigne variant avec le ramp on setpoint
+
+      ecart1 = fmod((capConsigne - capActuel_filt + 180.0), 360.0) - 180.0;    //calcul de l'ecart entre le cap actuel et le cap consigne variant avec le ramp on setpoint
+      
+      if (ecart1 < -180.0){
+        ecart1 += 360.0;
+      }
+      
+      ui = ui + (kp*dt*ecart1)/Ti    ;                   //calcul de la commande intégrale
+      commande = kp*ecart1 + ui  ;                    // calcul de la commande    //calcul de la commande PI
+
+      
+     
+    }
+    if (modeAuto ==false){                            //Mode MANU
+      float val ;                           
+      //float commandeManu ;                   //commande du mode en manu
+      val = analogRead(3);                   //calcul de la commande en MANU grace au potentiometre qui pourrait varier entre -255 et +255
+      commande = map(val,0,1023,-255,255) ;
+
+      // dispositif auto <--> manu sans à coup
+      capConsigne = capActuel_filt;
+      capPrecedent= capActuel_filt;
+      ui = ( float ) commande;
+    }
+
+    // Ajustement du rapport cyclique du hacheur (0 => 0 ; 255 =>100%) gauche et droite selon l’erreur de cap
+    
+    int alpha = constrain(abs(commande), 0, alpha_bin);
+    digitalWrite(brakePin1, LOW); // Désactiver le frein
+    digitalWrite(brakePin2, LOW); // Désactiver le frein
+    
+    // Inversion des directions pour une rotation sur place
+    if (commande > 0) {
+      digitalWrite(directionPin1, HIGH); // Moteur 1 avant
+      digitalWrite(directionPin2, HIGH);  // Moteur 2 arrière
+    } else {
+      digitalWrite(directionPin1, LOW);  // Moteur 1 arrière
+      digitalWrite(directionPin2, LOW); // Moteur 2 avant
+    }
+    
+    analogWrite(pwmPin1, alpha);
+    analogWrite(pwmPin2, alpha);  
+
+    // Affichage des données
+    //Serial.print("Cap Actuel : ");
+    //Serial.print(capActuel);
+    //Serial.print("Cap Actuel : ");
+    //Serial.print(",");
+    Serial.print(capActuel_filt);;
+    Serial.print(",");
+    Serial.print(capConsigne);
+    Serial.print(",");
+    Serial.print(kp);
+    Serial.print(",");
+    Serial.print(Ti);
+    Serial.print(",");
+    Serial.println(alpha_pourcent);
+    //Serial.print(" | Cap Cible : "); 
+    //Serial.print(","); Serial.println(ecart1);
+    //Serial.print(","); Serial.print(ecart1);
+    //Serial.print(","); Serial.println( commande );
+    //Serial.println( commande );
+
+  }
+}
